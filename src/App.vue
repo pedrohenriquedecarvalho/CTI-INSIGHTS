e6ce7214df201e2de0e5ba1f0d30f44e31af1467
Pedro Henrique de Carvalho <45061290836@>
2026-09-03 22:59:55 -0300

    Resolve merge conflicts and keep local version

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 <template>
 <!-- O router-view renderiza a HomeView, LoginView ou o AppLayout dependendo da URL -->
 <router-view />
+=======
+<script setup>
+
+import home from './components/home.vue'
+</script>
+
+<template>
+    <home/>
+>>>>>>> 8ee5a4dbd502c35a3e650d9e58dad9ea68af681a
 </template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
+<<<<<<< HEAD
 import App from './App.vue'
 import router from './router/index.js' // Importa as rotas de src/router/index.js
 import './style.css' // Importa o Tailwind CSS (se houver)
 const app = createApp(App)
 app.use(router) // Registra o Vue Router na aplicação
 app.mount("#app")
+=======
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+>>>>>>> 8ee5a4dbd502c35a3e650d9e58dad9ea68af681a
